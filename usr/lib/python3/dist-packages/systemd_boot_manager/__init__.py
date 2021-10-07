967d80940ddfef4dc6e7002790a28ecc10264d6c
Thomas Castleman <contact@draugeros.org>
2021-10-06 23:15:53 -0400

    add systemd service to enforce boot entry

--- usr/lib/python3/dist-packages/systemd_boot_manager/__init__.py
+++ usr/lib/python3/dist-packages/systemd_boot_manager/__init__.py
@@ -21,9 +21,10 @@
 #  MA 02110-1301, USA.
 #
 #
-"""Explain what this program does here!!!"""
+"""Library for systemd-boot-manager"""
 import os
 import sys
+import subprocess
 
 
 GREEN = "\033[92m"
@@ -53,11 +54,117 @@ def is_enabled():
             with open(CONFIG_DIR + "/enabled.conf", "w") as file:
                 file.write("enabled")
         except PermissionError:
-            eprint(f"{ ERROR }{ CONFIG_DIR }/enabled.conf does not exist.")
-            eprint(f"{ ERROR }This defaults systemd-boot-manager to enabled, but")
-            eprint(f"{ ERROR }the file cannot be created to allow changing of this setting.")
-            eprint(f"{ ERROR }Try running `sudo systemd-boot-manager --repair'.")
+            eprint(f"{ ERROR }{ CONFIG_DIR }/enabled.conf does not exist.{ CLEAR }")
+            eprint(f"{ ERROR }This defaults systemd-boot-manager to enabled, but{ CLEAR }")
+            eprint(f"{ ERROR }the file cannot be created to allow changing of this setting.{ CLEAR }")
+            eprint(f"{ ERROR }Try running `sudo systemd-boot-manager --repair'.{ CLEAR }")
         finally:
             return True
     with open(CONFIG_DIR + "/enabled.conf", "r") as file:
         return bool(file.read())
+
+
+def update_defaults_file(default_entry, verbose=False):
+    """Write default bootloader entry to config file"""
+    DEFAULTS_FILE_ADDON = """# This file defines the default boot entry that systemd-boot should be using.
+    # It only reads the first line, but these other lines are being commented out like
+    # in a shell script because if this file gets renamed and used for other purposes,
+    # then this syntax will be honored
+    #
+    # That first line needs to be the Entry ID you want to be default from
+    # `bootctl list`
+    #
+    # If you don't want systemd-boot-manager changing the default entry, just comment that
+    # first line out and it will not do anything."""
+    try:
+        if verbose:
+            print("Writing default boot entry to " + DEFAULTS_FILE + " . . .")
+        with open(DEFAULTS_FILE, "w") as conf:
+            if default_entry is not None:
+                conf.write(default_entry)
+            else:
+                if verbose:
+                    print("No default set, writing dummy value . . .")
+                conf.write("# default_entry_here.conf")
+            conf.write("\n")
+            conf.write(DEFAULTS_FILE_ADDON)
+    except (FileNotFoundError, PermissionError) as err:
+        eprint(ERROR + "An Unwarrented error has occured. Please try again later." + CLEAR)
+        sys.exit(2)
+
+
+def get_boot_entries(verbose=False):
+    """Get bootloader entries"""
+    if verbose:
+        print("Getting boot entry list . . .")
+    entries = subprocess.check_output(["bootctl", "list", "--no-pager"]).decode().split("\n")
+    output = {}
+    name = ""
+    if verbose:
+        print("Parsing List. . .")
+    for each in entries:
+        if "title: " in each:
+            if "(default)" in each:
+                output[" ".join(each.split()[1:])[:-10]] = {"default":True}
+                name = " ".join(each.split()[1:])[:-10]
+            else:
+                output[" ".join(each.split()[1:])] = {"default":False}
+                name = " ".join(each.split()[1:])
+        elif "id: " in each:
+            output[name]["id"] = each.split()[-1]
+    return output
+
+
+def get_default_boot_entry(verbose):
+    """Get default bootloader entry"""
+    entries = get_boot_entries(verbose=verbose)
+    if verbose:
+        print("Checking parsed data for 'default' flag . . .")
+    for each in entries:
+        if entries[each]["default"] is True:
+            return entries[each]["id"]
+
+
+def set_as_default_entry(entry, edit_file=True):
+    """Set 'entry' as the default bootloader entry"""
+    entries = get_boot_entries()
+    for each in entries:
+        if entries[each]["id"] == entry:
+            if entries[each]["default"] is True:
+                eprint(BLUE + "Already Default Entry" + CLEAR)
+                sys.exit()
+            # Set as default here
+            if edit_file:
+                update_defaults_file(entry)
+            try:
+                subprocess.check_call(["bootctl", "set-default", entry])
+            except subprocess.CalledProcessError as err:
+                eprint(ERROR + "CANNOT SET INTENDED DEFAULT" + CLEAR)
+                eprint("Error was:")
+                eprint(err.output)
+                sys.exit(err.returncode)
+            print(GREEN + "SUCCESS!" + CLEAR)
+            sys.exit()
+    eprint(ERROR + "ID Not found. Please provide a valid ID. IDs can be found using `update-systemd-boot --list'.")
+
+
+def read_defaults_file():
+    """Read Defaults file and return the default"""
+    with open(DEFAULTS_FILE, "r") as conf:
+        return conf.read().split()[0]
+
+
+def check_default_entry(verbose=False):
+    """Confirm default boot entry is correct"""
+    if verbose:
+        print("Reading defaults file . . .")
+    intended_default = read_defaults_file()
+    default = get_default_boot_entry(verbose)
+    if verbose:
+        print("Comparing defaults file to defaults in memory . . .")
+    if default == intended_default:
+        return True
+    elif intended_default == "#":
+        return True
+    else:
+        return False
---
+++ usr/share/systemd-boot-manager/service.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  service.py
+#
+#  Copyright 2021 Thomas Castleman <contact@draugeros.org>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+"""Systemd service to check the default boot entry and change it, if enabled"""
+import systemd_boot_manager
+
+# If it isn't enabled, don't do anything
+if systemd_boot_manager.is_enabled():
+    # Default entry does not match. Do something
+    if not systemd_boot_manager.check_default_entry():
+        systemd_boot_manager.eprint(f"{ systemd_boot_manager.ERROR }UEFI-CONFIGURED DEFAULT BOOT ENTRY DOES NOT MATCH DEFAULT ON FILE{ systemd_boot_manager.CLEAR }")
+        systemd_boot_manager.eprint(f"{ systemd_boot_manager.ERROR }CHANGING TO MATCH ENTRY ON FILE{ systemd_boot_manager.CLEAR }")
+        intended_default = systemd_boot_manager.read_defaults_file()
+        systemd_boot_manager.set_as_default_entry(intended_default, edit_file=False)
